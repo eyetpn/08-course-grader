e7c8ba9a33c0dc83e8194ca3a386fb56d35e854d
eyetpn <eyetpn@gmail.com>
2019-04-21 14:23:35 -0600

    Update course_grader.py

--- course_grader.py
+++ course_grader.py
@@ -1,2 +1,22 @@
-def course_grader(param):
-    return ""
+def average(n):
+    ave = sum(n)/ len (n)
+    return ave
+average 
+def course_grader(test_scores):
+    # Your code here
+    if average (test_scores) >= 70:
+        #if test_scores > 49:
+        return ("pass")
+    elif average (test_scores) < 70 and test_scores < 50:
+        print( "fail")
+    
+def main():
+    print(course_grader([100,75,45]))     # "fail"
+    print(course_grader([100,70,85]))     # "pass"
+    print(course_grader([80,60,60]))      # "fail"
+    print(course_grader([80,80,90,30,80]))  # "fail"
+    print(course_grader([70,70,70,70,70]))  # "pass"
+    print(course_grader([60,80,80,70,70]))  # "pass"
+
+if __name__ == "__main__":
+    main()
